d8d53b0be5790452c59d3cb711563f56e7175f98
rahul-1419 <rahulchaudhari1452003@gmail.com>
2025-09-30 15:24:31 +0530

    complete

--- app.py
+++ app.py
@@ -2,19 +2,35 @@ import pandas as pd
 import numpy as np
 import pickle
 from flask import Flask, request, render_template
+from sklearn.preprocessing import StandardScaler
+sclr = StandardScaler()
 
 # load pickle model
 
 model = pickle.load(open('model.pkl','rb'))
+sclr = pickle.load(open('scaler.pkl','rb'))
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', method=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    pass
+    Open = request.form['Open']
+    High = request.form['High']
+    Low = request.form['Low']
+    Adj_close = request.form['Adj_close']
+    Volume = request.form['Volume']
+    Year = request.form['Year']
+    Month = request.form['Month']
+    Day = request.form['Day']
+
+    features = np.array([[Open,High,Low,Adj_close,Volume,Year,Month,Day]])
+    features = sclr.transform(features)
+    prediction = model.predict(features).reshape(1,-1)
+    
+    return render_template('index.html',output = prediction[0])
 
 #main py
 
